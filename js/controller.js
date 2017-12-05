77fdc66d506e08170798c35822b7898cf72190ce
Ronny Valdivieso <ron_251@hotmail.com>
2017-12-05 07:51:48 -0500

    date validatio

---
+++ js/controller.js
@@ -0,0 +1,13 @@
+angular.module('dateInputExample', ['ui.bootstrap'])
+.controller('DateController', ['$scope', function($scope) {
+
+  $scope.validate = function(value) {
+    var today = new Date();
+
+    if (today.setHours(0) <= value.setHours(0)) {
+      alert("OK");
+    } else {
+      alert("Mayor");
+    }
+  };
+}]);
